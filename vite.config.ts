10266e131f1ae692b092f26ca5bd3cd0e50217c5
ZhengYihan <zhengyihan@example.com>
2025-09-08 15:17:12 +0800

    更新github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/smart-report/' : '/',
+  base: process.env.GITHUB_PAGES ? '/smart-report/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
